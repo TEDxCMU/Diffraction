198292407af0a38dd9dac9d923b1e2c7c8911373
Michelle <mliuu3259@gmail.com>
2023-01-30 21:42:22 -0500

    initial styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,40 +1,32 @@
 import Link from 'next/link';
 import styles from 'components/navbar.module.css';
-import Logo from 'assets/tedxcmu-logo.svg';
+import cn from 'classnames';
 
 function NavBar() {
     return (
         <nav className={styles.container}>
-            <Logo className={styles.logo} />
+            <Link href="/">
+                <p className={styles.logo}>TEDxCMU</p>
+            </Link>
             <div className={styles.links}>
-                <Link href="/">
-                    <a className={styles.link}>
-                        Home
-                    </a>
-                </Link>
-                <Link href="/stage">
-                    <a className={styles.link}>
-                        Stage
-                    </a>
-                </Link>
                 <Link href="/schedule">
-                    <a className={styles.link}>
-                        Schedule
-                    </a>
+                    <a className={styles.link}>Schedule</a>
                 </Link>
                 <Link href="/speakers">
-                    <a className={styles.link}>
-                        Speakers
-                    </a>
+                    <a className={styles.link}>Speakers</a>
                 </Link>
                 <Link href="/expo">
-                    <a className={styles.link}>
-                        Innovation Expo
+                    <a className={styles.link}>Innovation Expo</a>
+                </Link>
+                <Link href="/about">
+                    <a className={styles.link}>About</a>
+                </Link>
+                <Link href="https://www.tedxcmu.org/">
+                    <a target="_blank" className={cn(styles.link, styles.btn)}>
+                        Purchase Tickets
                     </a>
                 </Link>
             </div>
-            {/* Empty div for spacing. */}
-            <div />
         </nav>
     );
 }
