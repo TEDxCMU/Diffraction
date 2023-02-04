f2183202a3f66d430e69068a0ea8dd8296dff65a
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-02-04 17:46:45 -0500

    updated to react 18 to work with threejs packages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,23 +5,23 @@ import styles from 'components/navbar.module.css';
 function NavBar() {
     return (
         <nav className={styles.container}>
-            <Link href="/">
+            <Link legacyBehavior href="/">
                 <p className={styles.logo}>TEDxCMU</p>
             </Link>
             <div className={styles.links}>
-                <Link href="/schedule">
+                <Link legacyBehavior href="/schedule">
                     <a className={styles.link}>Schedule</a>
                 </Link>
-                <Link href="/speakers">
+                <Link legacyBehavior href="/speakers">
                     <a className={styles.link}>Speakers</a>
                 </Link>
-                <Link href="/expo">
+                <Link legacyBehavior href="/expo">
                     <a className={styles.link}>Innovation Expo</a>
                 </Link>
-                <Link href="/about">
+                <Link legacyBehavior href="/about">
                     <a className={styles.link}>About</a>
                 </Link>
-                <Link href="https://www.tedxcmu.org/">
+                <Link legacyBehavior href="https://www.tedxcmu.org/">
                     {/* TODO: change to real link later */}
                     {/* <a target="_blank" className={cn(styles.link, styles.btn)}>
                         Purchase Tickets
