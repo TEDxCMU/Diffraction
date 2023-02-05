462c0f4a258b70512ba44cf623d65841627f311b
Michelle <mliuu3259@gmail.com>
2023-02-04 19:23:24 -0500

    got homepage info loaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
+import styles from './index.module.css';
+
 function Home() {
-  return <div>TEDxCMU: Diffraction</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <div className={styles.eventInfo}>
+          <h1 className="heading">Diffraction</h1>
+          <p className="heading">Location</p>
+          <p className="heading">DATE + TIME</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
