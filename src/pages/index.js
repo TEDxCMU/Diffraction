f0c86bbb9185eb5b333645098d66aa27c5c2c02f
Michelle <mliuu3259@gmail.com>
2023-02-24 09:49:39 -0500

    adjust font sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function Home() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.headerContainer}>
-        <div className={styles.eventInfo}>
-          <h1 className="heading">Diffraction</h1>
-          <p className="heading">Simmons Auditorium</p>
-          <p className="heading">March 25, 7 pm EST</p>
+    return (
+        <div className={styles.container}>
+            <div className={styles.headerContainer}>
+                <div className={styles.eventInfo}>
+                    <h1 className="largeTitle">diffraction</h1>
+                    <p className="heading">Simmons Auditorium</p>
+                    <p className="heading">March 25, 7 pm EST</p>
+                </div>
+            </div>
+            <div className={styles.talks}>
+                <p className="subheading">TALKS</p>
+            </div>
         </div>
-      </div>
-      <div className={styles.talks}>
-        <p className="subheading">TALKS</p>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
