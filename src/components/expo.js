9ad5396e19b59e7175579b11a208bbc18d52c4b6
Michelle <mliuu3259@gmail.com>
2023-02-25 14:27:15 -0500

    updated styling

--- src/components/expo.js
+++ src/components/expo.js
@@ -1,12 +1,10 @@
-import { useState, useEffect } from 'react';
-import { getInnovators } from 'utils/content';
-import styles from 'components/expo.module.css';
-import InnovatorGrid from 'components/innovator-grid';
-
+import { useState, useEffect } from "react";
+import { getInnovators } from "utils/content";
+import styles from "components/expo.module.css";
+import InnovatorGrid from "components/innovator-grid";
 
 function Expo() {
     const [data, setData] = useState([]);
-
 
     useEffect(() => {
         init();
@@ -14,22 +12,20 @@ function Expo() {
 
     async function init() {
         const content = await getInnovators();
-        setData(content)
+        setData(content);
     }
 
     console.log(data);
 
     return (
-        <> 
+        <>
             {/* <Background /> */}
             <main className={styles.container}>
-                <h1 className={styles.header}>
-                    Innovation Expo
-                </h1>
+                <h1 className="title">Speakers</h1>
                 <p className={styles.description}>
                     Short Description of what Innovation Expo is
                 </p>
-                <InnovatorGrid innovators={data}/>
+                <InnovatorGrid innovators={data} />
             </main>
         </>
     );
